1c973296ae008707132539f689201569f766dc14
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-06-07 14:58:23 +0900

    最適化方法を追加

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@ typedef struct {
   ld y;
 } coordinate_t;
 
+ld calculateDistance(const coordinate_t from, const coordinate_t to);
 void greedySearch(unordered_map<int, coordinate_t> data, vector<int>& path);
 bool doTwoOpt(const unordered_map<int, coordinate_t>& data, vector<int>& path);
+bool swapFivePoint(const unordered_map<int, coordinate_t>& data, vector<int>& path, const int index);
 bool isPathCrossing(const coordinate_t from1, const coordinate_t to1, const coordinate_t from2, const coordinate_t to2);
 void readInput(unordered_map<int, coordinate_t>& data, const string targetDataNum);
 void outputCsv(const vector<int>& path, const string targetDataNum);
@@ -21,14 +23,24 @@ int main(int argc, char *argv[]) {
   readInput(data, targetDataNum);
   greedySearch(data, path);
 
+  default_random_engine eng((int)time(NULL));
+  uniform_int_distribution<int> distr(0, (int)data.size() - 5);
+
   bool isTwoOptDone = true;
+  bool isSwapFourPointDone = true;
   int count = 0;
-  while (isTwoOptDone && count < (int)(data.size() * 2)) {
+  while ((isTwoOptDone || isSwapFourPointDone) && count < (int)(data.size() * 2)) {
     isTwoOptDone = doTwoOpt(data, path);
+    isSwapFourPointDone = swapFivePoint(data, path, distr(eng));
     count++;
   }
 
   outputCsv(path, targetDataNum);
+}
+
+/** from から to までの距離の二乗を計算 */
+ld calculateDistance(const coordinate_t from, const coordinate_t to) {
+  return ((from.x - to.x) * (from.x - to.x)) + ((from.y - to.y) * (from.y - to.y));
 }
 
 /** 貪欲法で経路検索 */
@@ -44,7 +56,7 @@ void greedySearch(unordered_map<int, coordinate_t> data, vector<int>& path) {
       if (itr->first == fromIndex) continue;
 
       coordinate_t to = itr->second;
-      distance = ((from.x - to.x) * (from.x - to.x)) + ((from.y - to.y) * (from.y - to.y));
+      distance = calculateDistance(from, to);
 
       if (minDistance == -1) {
         minDistance = distance;
@@ -104,16 +116,39 @@ bool doTwoOpt(const unordered_map<int, coordinate_t>& data, vector<int>& path) {
   return false;
 }
 
+/** 5 点を結ぶパスについて最適化 */
+bool swapFivePoint(const unordered_map<int, coordinate_t>& data, vector<int>& path, const int index) {
+  coordinate_t first = data.at(path[index]);
+  coordinate_t second = data.at(path[index + 1]);
+  coordinate_t third = data.at(path[index + 2]);
+  coordinate_t forth = data.at(path[index + 3]);
+  coordinate_t fifth = data.at(path[index + 4]);
+
+  ld currentDistance = calculateDistance(first, second) + calculateDistance(second, third) + calculateDistance(third, forth) + calculateDistance(forth, fifth);
+  ld anotherDistance = calculateDistance(first, third) + calculateDistance(third, forth) + calculateDistance(forth, second) + calculateDistance(second, fifth);
+
+  if (anotherDistance < currentDistance) {
+    int swap = path[index + 1];
+    path[index + 1] = path[index + 2];
+    path[index + 2] = path[index + 3];
+    path[index + 3] = swap;
+    return true;
+  }
+  return false;
+}
+
 /** from1 と to1 を結ぶパスと from2 と to2 を結ぶパスがクロスしているかどうかを返す関数 */
 bool isPathCrossing(const coordinate_t from1, const coordinate_t to1, const coordinate_t from2, const coordinate_t to2) {
-  if (
-    ((from1.x - to1.x) * ( from2.y - from1.y) + (from1.y - to1.y) * (from1.x - from2.x))
-      * ((from1.x - to1.x) * ( to2.y - from1.y) + (from1.y - to1.y) * (from1.x - to2.x)) < 0 &&
-    ((from2.x - to2.x) * (from1.y - from2.y) + (from2.y - to2.y) * (from2.x - from1.x))
-      * ((from2.x - to2.x) * (to1.y - from2.y) + (from2.y - to2.y) * (from2.x - to1.x)) < 0
-  ) return true;
+  ld s, t;
+  s = (from1.x - to1.x) * (from2.y - from1.y) - (from1.y - to1.y) * (from2.x - from1.x);
+  t = (from1.x - to1.x) * (to2.y - from1.y) - (from1.y - to1.y) * (to2.x - from1.x);
+  if (s * t >= 0) return false;
 
-  return false;
+  s = (from2.x - to2.x) * (from1.y - from2.y) - (from2.y - to2.y) * (from1.x - from2.x);
+  t = (from2.x - to2.x) * (to1.y - from2.y) - (from2.y - to2.y) * (to1.x - from2.x);
+  if (s * t >= 0) return false;
+    
+  return true;
 }
 
 /** 座標データの読み込み */
